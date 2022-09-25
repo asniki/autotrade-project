ddc0f3a7a63a12bef870f72c4930a87fa4c6619a
asniki <i38532ii@gmail.com>
2022-09-25 11:08:31 +0300

    update

--- autotrade-service/src/main/java/com/autotrade/service/controller/TestController.java
+++ autotrade-service/src/main/java/com/autotrade/service/controller/TestController.java
@@ -14,6 +14,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -164,7 +165,8 @@ public class TestController {
         }
     }
 
-    @GetMapping("/test")
+//    @GetMapping("/test")
+    @RequestMapping("/test")
     public ResponseEntity<String> test() throws JsonProcessingException {
         return ResponseEntity.ok(objectMapper.writeValueAsString("123"));
     }
